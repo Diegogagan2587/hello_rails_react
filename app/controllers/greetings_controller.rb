c3a73ce20c1e32bf96e9b979ef8a3b7b26c7829f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-31 15:14:53 -0600

    Add responses for different formats: html, xml, json

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,13 @@
 class GreetingsController < ApplicationController
   def index
+    # first i get a random Greeting id
+    random_greeting_id = Greeting.pluck(:id).sample
+    @greeting_random = Greeting.find(random_greeting_id)
+
+    respond_to do | format |
+      format.html
+      format.json { render json: @greeting_random }
+      format.xml { render xml: @greeting_random }
+    end
   end
 end
