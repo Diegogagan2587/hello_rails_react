bff219a6a93033879ca762f059c31c2e8324275b
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-01 06:57:12 -0600

    Add API-endpoint: Create new route for the API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
 end
