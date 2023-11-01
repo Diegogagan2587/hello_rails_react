e084cb06cfc80a6a7056e481f087c3e62719d34c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-01 13:04:06 -0600

    Render app component using React router

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,25 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-
+import '@hotwired/turbo-rails';
+import './controllers';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
-  return (<h1>Hello World! from React Component</h1>);
+  return <h1>Hello World! from React Component</h1>;
 }
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
