9e1f4a2b4c4f530a2f98120dc10d36cd7009a25e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-01 23:48:27 -0600

    Fix sintax error

--- app/javascript/components/Greetings.js
+++ app/javascript/components/Greetings.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Greetings() {
+  const randomGreeting = useSelector((state) => state);
   return (
-    <p>Message: </p>
+    <>
+      <p>Message: </p>
+      <p>{message}</p>
+    </>
   );
 }
 
